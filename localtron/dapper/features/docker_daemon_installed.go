48522fe3e6c70293f36918c7a0747619d87067b2
Janos Dobronszki <dobronszki@gmail.com>
2024-05-29 20:36:20 +0200

    dapper: Fix backslash issue in feature DockerDaemonInstalled on Linux (#6)

--- localtron/dapper/features/docker_daemon_installed.go
+++ localtron/dapper/features/docker_daemon_installed.go
@@ -21,11 +21,11 @@ var DockerDaemonInstalled = dt.Feature{
 				Source: `
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS
 sudo apt-get update
-sudo apt-get install \\
-	apt-transport-https \\
-	ca-certificates \\
-	curl \\
-	gnupg-agent \\
+sudo apt-get install \
+	apt-transport-https \
+	ca-certificates \
+	curl \
+	gnupg-agent \
 	software-properties-common -y
 
 # Add Docker’s official GPG key
@@ -35,9 +35,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
 # Set up the stable repository
-sudo add-apt-repository \\
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \\
-   $(lsb_release -cs) \\
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
    stable"
 
 # Update the apt package index, and install the latest version of Docker Engine and containerd
