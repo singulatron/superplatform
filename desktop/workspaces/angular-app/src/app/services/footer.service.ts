aa51e06699401dd1d2e7dc8ef5e97bde2282a75c
crufter <dobronszki@gmail.com>
2024-06-26 12:19:11 +0200

    frontend: Fix footer not being removed when changing pages

--- desktop/workspaces/angular-app/src/app/services/footer.service.ts
+++ desktop/workspaces/angular-app/src/app/services/footer.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, Type, ComponentRef } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { map, filter } from 'rxjs/operators';
+import { ChatInputComponent } from '../chat/chat-box/chat-input/chat-input.component';
 
 @Injectable({
 	providedIn: 'root',
@@ -12,7 +15,45 @@ export class FooterService {
 	footerComponentSubject = new ReplaySubject<Type<any> | null>(1);
 	footerComponent$ = this.footerComponentSubject.asObservable();
 
-	constructor() {}
+	constructor(
+		private activatedRoute: ActivatedRoute,
+		private router: Router
+	) {
+		// Since ionic lifecycle hooks dont seem to
+		// be triggering properly - nor ngonint
+		// we need to do this hack here
+		// Idealy we would do something like this
+		// in the chat box component:
+		//  this.subscriptions.push(
+		//  	this.mobile.isMobile$.subscribe((isMobile) => {
+		//  		if (isMobile) {
+		//  			this.footer.updateFooterComponent(ChatInputComponent);
+		//  		}
+		//  	})
+		//  );
+		// and then unscubscribe in ngOnDestroy.
+		this.router.events
+			.pipe(
+				filter((event) => event instanceof NavigationEnd),
+				map(() => this.activatedRoute),
+				map((route) => {
+					while (route.firstChild) route = route.firstChild;
+					return route;
+				}),
+				filter((route) => route.outlet === 'primary'),
+				map((route) => route.snapshot.url.join('/'))
+			)
+			.subscribe((path) => {
+				console.log('path is', path);
+				if (path === 'chat') {
+					console.log('updating footer to ChatInputComponent');
+					this.updateFooterComponent(ChatInputComponent);
+				} else {
+					console.log('removing footer');
+					this.removeFooterComponent();
+				}
+			});
+	}
 
 	hasFooterComponent(): boolean {
 		return this.hasFooter;
