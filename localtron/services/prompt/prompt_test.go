5d313a5691bcd0b3306c3368bd54404ebef2355f
crufter <dobronszki@gmail.com>
2024-07-02 09:31:29 +0200

    tests: Improvements

---
+++ localtron/services/prompt/prompt_test.go
@@ -0,0 +1,75 @@
+package promptservice_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+
+	"github.com/singulatron/singulatron/localtron/di"
+	chatservice "github.com/singulatron/singulatron/localtron/services/chat"
+	chattypes "github.com/singulatron/singulatron/localtron/services/chat/types"
+	configservice "github.com/singulatron/singulatron/localtron/services/config"
+	firehoseservice "github.com/singulatron/singulatron/localtron/services/firehose"
+	modelservice "github.com/singulatron/singulatron/localtron/services/model"
+	promptservice "github.com/singulatron/singulatron/localtron/services/prompt"
+	prompttypes "github.com/singulatron/singulatron/localtron/services/prompt/types"
+	userservice "github.com/singulatron/singulatron/localtron/services/user"
+)
+
+func TestMessageCreatesThread(t *testing.T) {
+	univ, err := di.MakeUniverse()
+	require.NoError(t, err)
+	ps := univ.PromptService
+
+	promptId := uuid.New().String()
+	t.Run("add prompt", func(t *testing.T) {
+		err := ps.AddPrompt(&prompttypes.Prompt{
+
+		})
+
+	t.Run("thread does not exist", func(t *testing.T) {
+		err := as.AddMessage(&chattypes.Message{
+			Id:       uuid.New().String(),
+			ThreadId: "1",
+			Content:  "hi there",
+		})
+		require.Error(t, err)
+	})
+
+	t.Run("no user id", func(t *testing.T) {
+		tid := uuid.New().String()
+		title := "Test Thread Title"
+		_, err := as.AddThread(&chattypes.Thread{
+			Id:    tid,
+			Title: title,
+		})
+		require.Error(t, err)
+	})
+
+	userId := "usr-1"
+	var threadId string
+
+	t.Run("create thread", func(t *testing.T) {
+		tid := uuid.New().String()
+		title := "Test Thread Title"
+		thread, err := as.AddThread(&chattypes.Thread{
+			Id:      tid,
+			Title:   title,
+			UserIds: []string{userId},
+		})
+		require.NoError(t, err)
+		require.Equal(t, tid, thread.Id)
+		require.Equal(t, title, thread.Title)
+		threadId = thread.Id
+	})
+
+	t.Run("no user id", func(t *testing.T) {
+		err := as.AddMessage(&chattypes.Message{
+			Id:       uuid.New().String(),
+			ThreadId: threadId,
+			Content:  "hi there",
+		})
+		require.NoError(t, err)
+	})
+}
