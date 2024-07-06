559296c1188a83cef73d8592815b4c565f1b20a5
crufter <dobronszki@gmail.com>
2024-07-06 09:49:30 +0200

    Checkpoint

---
+++ desktop/workspaces/angular-app/src/app/services/query.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Condition, equal, contains, startsWith, field } from './generic.service';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class QueryService {
+	constructor() {}
+}
+
+export class QueryParser {
+	parse(queryString: string): Query {
+		const query: Query = {};
+
+		// Match and parse fields
+		const fieldRegex = /(\w+):([~^]?[\w\d\-..]+)/g;
+		let match;
+		while ((match = fieldRegex.exec(queryString)) !== null) {
+			const field = match[1];
+			const value = match[2];
+
+			if (!query.conditions) query.conditions = [];
+			query.conditions.push(this.createCondition(field, value));
+		}
+
+		const orderByRegex = /orderBy:([\w,]+)/;
+		const orderByMatch = orderByRegex.exec(queryString);
+		if (orderByMatch) {
+			query.orderBy = orderByMatch[1].split(',').map((field) => {
+				const [fieldName, order] = field.split(':');
+				return { field: fieldName, desc: order === 'desc' };
+			});
+		}
+
+		const limitRegex = /limit:(\d+)/;
+		const limitMatch = limitRegex.exec(queryString);
+		if (limitMatch) {
+			query.limit = parseInt(limitMatch[1], 10);
+		}
+
+		const afterRegex = /after:([\w,]+)/;
+		const afterMatch = afterRegex.exec(queryString);
+		if (afterMatch) {
+			query.after = afterMatch[1].split(',');
+		}
+
+		return query;
+	}
+
+	private createCondition(fieldName: string, value: string): Condition {
+		if (value.startsWith('~')) {
+			return contains(field(fieldName), value.substring(1));
+		} else if (value.startsWith('^')) {
+			return startsWith(field(fieldName), value.substring(1));
+		} else {
+			return equal(field(fieldName), isNaN(value as any) ? value : parseFloat(value));
+		}
+	}
+}
+
+interface Query {
+	conditions?: Condition[];
+	orderBy?: { field: string, desc: boolean }[];
+	limit?: number;
+	after?: string[];
+}
