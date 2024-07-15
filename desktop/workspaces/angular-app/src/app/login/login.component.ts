b27f9cd152be92e9e079d02559dfb86410055ded
crufter <dobronszki@gmail.com>
2024-07-15 08:13:53 +0200

    chore: change license from agpl v3 only

--- desktop/workspaces/angular-app/src/app/login/login.component.ts
+++ desktop/workspaces/angular-app/src/app/login/login.component.ts
@@ -4,9 +4,6 @@
  *
  * This source code is licensed under the GNU Affero General Public License v3.0 (AGPLv3) for personal, non-commercial use.
  * You may obtain a copy of the AGPL v3.0 at https://www.gnu.org/licenses/agpl-3.0.html.
- *
- * For commercial use, a separate license must be obtained by purchasing from The Authors.
- * For commercial licensing inquiries, please contact The Authors listed in the AUTHORS file.
  */
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UserService, LoginResponse } from '../services/user.service';
@@ -55,7 +52,7 @@ export class LoginComponent {
 			const toast = await this.toast.create({
 				cssClass: 'white-text',
 				color: 'danger',
-				message: JSON.parse(error as string)?.error,
+				message: ,
 				duration: 5000,
 				position: 'middle',
 			});
