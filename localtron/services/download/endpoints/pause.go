a228bfc55fc404aae48327138fd05edae541de41
Janos Dobronszki <dobronszki@gmail.com>
2024-07-18 23:08:22 +0200

    fix: remove leftover license cruft (#145)

--- localtron/services/download/endpoints/pause.go
+++ localtron/services/download/endpoints/pause.go
@@ -2,7 +2,7 @@
  * @license
  * Copyright (c) The Authors (see the AUTHORS file)
  *
- * This source code is licensed under the GNU Affero General Public License v3.0 (AGPLv3) for personal, non-commercial use.
+ * This source code is licensed under the GNU Affero General Public License v3.0 (AGPLv3).
  * You may obtain a copy of the AGPL v3.0 at https://www.gnu.org/licenses/agpl-3.0.html.
  */
 package downloadendpoints
