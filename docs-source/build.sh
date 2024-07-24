0b0cc8dc22a234edd436b7bf88bf7d6705e1ef24
crufter <dobronszki@gmail.com>
2024-07-24 12:26:41 +0200

    docs: regenerate

--- docs-source/build.sh
+++ docs-source/build.sh
@@ -2,8 +2,8 @@ cd ../localtron
 swag init
 cd ../docs-source
 cp ../localtron/docs/swagger.yaml examples/singulatron.yaml
-npm run build
 yarn gen-api-docs singulatron
+npm run build
 rm -rf ../docs/*
 cp CNAME ../docs/CNAME
 cp -r ./build/* ../docs/
