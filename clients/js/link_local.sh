5d586dbde3aa3d3ddbc64f64360ae2ed9333bbbe
crufter <dobronszki@gmail.com>
2024-07-25 17:00:09 +0200

    link fix v2

--- clients/js/link_local.sh
+++ clients/js/link_local.sh
@@ -2,7 +2,28 @@
 
 set -e
 
-cd types; npm run build; npm link; cd ..
-cd node; npm link @singulatron/types; npm run build; npm link; cd ..
-cd client; npm link @singulatron/types; npm run build; npm link; cd ..
-cd client-example; npm link @singulatron/types; npm link @singulatron/client; npm run build
+cd types;
+npm install
+npm run build;
+npm link;
+cd ..
+
+cd node;
+npm link @singulatron/types;
+npm install
+npm run build;
+npm link;
+cd ..
+
+cd client;
+npm link @singulatron/types;
+npm install
+npm run build;
+npm link;
+cd ..
+
+cd client-example;
+npm link @singulatron/types;
+npm link @singulatron/client;
+npm install
+npm run build
