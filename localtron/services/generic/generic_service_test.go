8786128635bb18e9a0a88d8cd5da0d076a90a54d
crufter <dobronszki@gmail.com>
2024-07-31 08:19:46 +0200

    port generic tests

--- localtron/services/generic/generic_service_test.go
+++ localtron/services/generic/generic_service_test.go
@@ -1,6 +1,8 @@
 package genericservice_test
 
 import (
+	"context"
+	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
@@ -9,6 +11,7 @@ import (
 	"github.com/singulatron/singulatron/localtron/datastore"
 	"github.com/singulatron/singulatron/localtron/di"
 	generictypes "github.com/singulatron/singulatron/localtron/services/generic/types"
+	usertypes "github.com/singulatron/singulatron/localtron/services/user/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,171 +22,205 @@ func TestCreate(t *testing.T) {
 	table1 := "test_table" + uniq
 	table2 := "test_table2" + uniq
 
-	universe, err := di.BigBang(di.Options{
-		Test: true,
-	})
-	require.NoError(t, err)
-	service := universe.GenericService
+	hs := &di.HandlerSwitcher{}
+	server := httptest.NewServer(hs)
+	defer server.Close()
 
-	userId := "user_1"
-	otherUserId := "user_2"
+	options := &di.Options{
+		Test: true,
+		Url:  server.URL,
+	}
+	universe, starterFunc, err := di.BigBang(options)
+	require.NoError(t, err)
+
+	hs.UpdateHandler(universe)
+	router := options.Router
+
+	err = starterFunc()
+	require.NoError(t, err)
+
+	token, err := usertypes.RegisterUser(router, "someuser", "pw123", "Some name")
+	require.NoError(t, err)
+	user1Router := router.SetBearerToken(token)
+
+	token, err = usertypes.RegisterUser(router, "someuser2", "pw123", "Some name 2")
+	require.NoError(t, err)
+	user2Router := router.SetBearerToken(token)
 
 	uuid1 := uuid.New().String()
 	uuid2 := uuid.New().String()
 
 	obj := &generictypes.GenericObject{
 		GenericObjectCreateFields: generictypes.GenericObjectCreateFields{
-			Id:     uuid1,
-			Table:  table1,
-			Data:   map[string]interface{}{"key": "value"},
-			UserId: userId,
+			Id:    uuid1,
+			Table: table1,
+			Data:  map[string]interface{}{"key": "value"},
 		},
 		CreatedAt: time.Now().String(),
 	}
 
-	err = service.Create(&generictypes.CreateRequest{
+	err = user1Router.Post(context.Background(), "generic", "/create", &generictypes.CreateRequest{
 		Object: &obj.GenericObjectCreateFields,
-	})
+	}, nil)
 	require.NoError(t, err)
 
 	t.Run("user 1 can find its own private record", func(t *testing.T) {
-		res, err := service.Find(generictypes.FindOptions{
+		req := generictypes.FindRequest{
 			Table:  table1,
-			UserId: userId,
 			Public: false,
 			Query: &datastore.Query{
 				Conditions: []datastore.Condition{
 					datastore.All(),
 				},
 			},
-		})
+		}
+		rsp := generictypes.FindResponse{}
+		err = user1Router.Post(context.Background(), "generic", "/find", req, &rsp)
 		require.NoError(t, err)
-		require.Equal(t, 1, len(res))
-		require.Contains(t, res[0].Id, uuid1)
+		require.Equal(t, 1, len(rsp.Objects))
+		require.Contains(t, rsp.Objects[0].Id, uuid1)
 	})
 
 	obj2 := &generictypes.GenericObject{
 		GenericObjectCreateFields: generictypes.GenericObjectCreateFields{
-			Id:     uuid2,
-			Table:  table2,
-			Data:   map[string]interface{}{"key": "value"},
-			UserId: otherUserId,
+			Id:    uuid2,
+			Table: table2,
+			Data:  map[string]interface{}{"key": "value"},
 		},
 		CreatedAt: time.Now().String(),
 	}
 
-	err = service.Create(&generictypes.CreateRequest{
+	err = user2Router.Post(context.Background(), "generic", "/create", &generictypes.CreateRequest{
 		Object: &obj2.GenericObjectCreateFields,
-	})
+	}, nil)
 	require.NoError(t, err)
 
 	t.Run("user 2 can find its own private record", func(t *testing.T) {
-		res, err := service.Find(generictypes.FindOptions{
+		req := generictypes.FindRequest{
 			Table:  table2,
-			UserId: otherUserId,
 			Public: false,
 			Query: &datastore.Query{
 				Conditions: []datastore.Condition{
 					datastore.All(),
-				}}})
+				}},
+		}
+		rsp := generictypes.FindResponse{}
+		err = user2Router.Post(context.Background(), "generic", "/find", req, &rsp)
 		require.NoError(t, err)
-		require.Equal(t, 1, len(res))
-		require.Contains(t, res[0].Id, uuid2)
+		require.Equal(t, 1, len(rsp.Objects))
+		require.Contains(t, rsp.Objects[0].Id, uuid2)
 	})
 
 	t.Run("find private for user 1", func(t *testing.T) {
-		res, err := service.Find(generictypes.FindOptions{
+		req := generictypes.FindRequest{
 			Table:  table1,
-			UserId: userId,
 			Public: false,
 			Query: &datastore.Query{Conditions: []datastore.Condition{
 				datastore.Id(uuid1),
-			}}})
+			}},
+		}
+		rsp := generictypes.FindResponse{}
+		err = user1Router.Post(context.Background(), "generic", "/find", req, &rsp)
 		require.NoError(t, err)
-		require.Equal(t, 1, len(res))
-		require.Equal(t, res[0].Id, uuid1)
+		require.Equal(t, 1, len(rsp.Objects))
+		require.Equal(t, rsp.Objects[0].Id, uuid1)
 	})
 
 	t.Run("find public for user 1", func(t *testing.T) {
-		res, err := service.Find(generictypes.FindOptions{
+		req := generictypes.FindRequest{
 			Table:  table1,
-			UserId: userId,
 			Public: true,
 			Query: &datastore.Query{Conditions: []datastore.Condition{
 				datastore.Id(uuid1),
-			}}})
+			}},
+		}
+		rsp := generictypes.FindResponse{}
+		err = user1Router.Post(context.Background(), "generic", "/find", req, &rsp)
 		require.NoError(t, err)
-		require.Equal(t, 0, len(res))
+		require.Equal(t, 0, len(rsp.Objects))
 	})
 
 	t.Run("already exists", func(t *testing.T) {
-		err = service.Create(
+		err = user1Router.Post(context.Background(), "generic", "/create",
 			&generictypes.CreateRequest{
 				Object: &obj.GenericObjectCreateFields,
-			})
+			},
+			nil)
 		require.Error(t, err)
 	})
 
 	t.Run("user 1 cannot see record of user 2", func(t *testing.T) {
-		res, err := service.Find(generictypes.FindOptions{
+		req := generictypes.FindRequest{
 			Table:  table1,
-			UserId: userId,
 			Public: false,
 			Query: &datastore.Query{Conditions: []datastore.Condition{
 				datastore.Id(uuid2),
-			}}})
+			}},
+		}
+		rsp := generictypes.FindResponse{}
+		err = user1Router.Post(context.Background(), "generic", "/find", req, &rsp)
 		require.NoError(t, err)
-		require.Equal(t, 0, len(res))
+		require.Equal(t, 0, len(rsp.Objects))
 	})
 
 	t.Run("user 2 cannot update record of user 1", func(t *testing.T) {
-		obj.UserId = otherUserId
-		err = service.Upsert(&generictypes.UpsertRequest{
+		req := &generictypes.UpsertRequest{
 			Object: &obj.GenericObjectCreateFields,
-		})
+		}
+		err = user2Router.Post(context.Background(), "generic", "/upsert", req, nil)
 		// unauthorized
 		require.Error(t, err)
-		obj.UserId = userId
 	})
 
 	t.Run("user 1 can upsert its own reord", func(t *testing.T) {
-		err = service.Upsert(&generictypes.UpsertRequest{
+		req := &generictypes.UpsertRequest{
 			Object: &obj.GenericObjectCreateFields,
-		})
+		}
+		err = user1Router.Post(context.Background(), "generic", "/upsert", req, nil)
 		require.NoError(t, err)
 	})
 
 	t.Run("user 1 can find its own reord", func(t *testing.T) {
-		res, err := service.Find(generictypes.FindOptions{
+		req := generictypes.FindRequest{
 			Table:  table1,
-			UserId: userId,
 			Public: false,
 			Query: &datastore.Query{Conditions: []datastore.Condition{
 				datastore.All(),
-			}}})
+			}},
+		}
+		rsp := generictypes.FindResponse{}
+		err = user1Router.Post(context.Background(), "generic", "/find", req, &rsp)
 		require.NoError(t, err)
-		require.Equal(t, 1, len(res))
-		require.Contains(t, res[0].Id, uuid1)
+		require.Equal(t, 1, len(rsp.Objects))
+		require.Contains(t, rsp.Objects[0].Id, uuid1)
 	})
 
 	t.Run("user 2 cannot delete user 1's record", func(t *testing.T) {
-		err = service.Delete(table1, otherUserId, []datastore.Condition{
-			datastore.Id(obj.Id),
-		})
+		req := generictypes.DeleteRequest{
+			Table: table1,
+			Conditions: []datastore.Condition{
+				datastore.Id(obj.Id),
+			},
+		}
+
+		err = user2Router.Post(context.Background(), "generic", "/delete", req, nil)
 		// no unauthorized but no error either...
 		require.NoError(t, err)
 	})
 
 	// ...item wont be deleted
 	t.Run("user 2 will no see other tables", func(t *testing.T) {
-		res, err := service.Find(generictypes.FindOptions{
+		req := generictypes.FindRequest{
 			Table:  table1,
-			UserId: otherUserId,
 			Public: false,
 			Query: &datastore.Query{Conditions: []datastore.Condition{
 				datastore.All(),
-			}}})
+			}},
+		}
+		rsp := generictypes.FindResponse{}
+		err = user2Router.Post(context.Background(), "generic", "/find", req, &rsp)
+
 		require.NoError(t, err)
-		require.Equal(t, 0, len(res))
+		require.Equal(t, 0, len(rsp.Objects))
 	})
 }
