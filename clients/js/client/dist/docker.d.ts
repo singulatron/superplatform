8d278d33cf8f1714e9eafaacdcf38e2d8e2f585b
crufter <dobronszki@gmail.com>
2024-08-07 19:34:07 +0200

    fixes

--- clients/js/client/dist/docker.d.ts
+++
@@ -1,8 +0,0 @@
-import { ClientOptions } from "./util";
-import * as docker from "@singulatron/types";
-export declare class DockerService {
-    private options;
-    constructor(options: ClientOptions);
-    call(endpoint: string, request: any): Promise<any>;
-    dockerInfo(): Promise<docker.DockerInfoResponse>;
-}
