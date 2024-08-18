e7835b9a3cb2795f2a90879db49c96b5ca7e468f
crufter <dobronszki@gmail.com>
2024-08-18 11:03:13 +0200

    regen

--- clients/go/gen.sh
+++ clients/go/gen.sh
@@ -10,6 +10,7 @@ cd "$LOCALTRON_DIR"
 swag init --parseDependency
 
 cd "$SCRIPT_DIR"
+rm -rf *.go
 openapi-generator-cli generate -i "$LOCALTRON_DIR/docs/swagger.yaml" -g go -o .
 rm -rf api docs go.mod
 cp go.mod.template go.mod
